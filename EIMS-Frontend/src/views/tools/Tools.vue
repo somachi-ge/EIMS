9d12900f705d6281600638500345f442ad087de2
gemac <gemac@example.com>
2026-03-28 11:06:52 +0800

    chore: 工作流更改为工作流程

--- EIMS-Frontend/src/views/tools/Tools.vue
+++ EIMS-Frontend/src/views/tools/Tools.vue
@@ -47,7 +47,7 @@ interface Application {
 const APPLICATIONS: readonly Application[] = [
   { id: 'code', name: '编码规则', icon: CodeOutlined, color: '#cf1322', path: '/coding-rule' },
   { id: 'dictionary', name: '数据字典', icon: BookOutlined, color: '#faad14', path: '/dictionary' },
-  { id: 'workflow', name: '工作流', icon: SwapOutlined, color: '#0958d9', path: '/workflow' },
+  { id: 'workflow', name: '工作流程', icon: SwapOutlined, color: '#0958d9', path: '/workflow' },
   { id: 'organization', name: '组织管理', icon: TeamOutlined, color: '#7cb305', path: '/system/organization' },
 ]
 
